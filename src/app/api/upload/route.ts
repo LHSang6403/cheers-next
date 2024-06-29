0470844bc17fbbd231a5f3fa4b60ecb15d23f9b2
LHSang <lhsang64.contact@gmail.com>
2024-06-29 13:38:20 +0700

    feat: api routes

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,39 @@
+import createSupabaseServerClient from "@/supabase/server";
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  try {
+    const supabase = await createSupabaseServerClient();
+
+    const file = req.body;
+    const filename = req.headers.get("x-vercel-filename") || "file.txt";
+    const contentType = req.headers.get("content-type") || "text/plain";
+    const fileType = `.${contentType.split("/")[1]}`;
+
+    const finalName = filename.includes(fileType)
+      ? filename
+      : `${filename}${fileType}`;
+
+    const { data, error } = await supabase.storage
+      .from("public_files")
+      .upload("/product_description/" + finalName, file!, {
+        // cacheControl: "no-cache",
+        upsert: true,
+        duplex: "half",
+      });
+
+    if (error) {
+      return new Response("Lỗi máy chủ", { status: 500 });
+    }
+
+    return Response.json({
+      data: "ok",
+      url:
+        "https://ieocflhkecudisgjolkc.supabase.co/storage/v1/object/public/images/" +
+        data!.path,
+    });
+  } catch (error) {
+    return new Response("Lỗi máy chủ", { status: 500 });
+  }
+}
