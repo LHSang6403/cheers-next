d86abd4675f9fcde8e8b578e75cbb07c24027e7a
LHSang <lhsang64.contact@gmail.com>
2024-06-30 12:59:06 +0700

    done

--- src/app/(main)/file/page.tsx
+++ src/app/(main)/file/page.tsx
@@ -6,10 +6,98 @@ import DropAndDragZone from "@/components/File/DropAndDragZone";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import axios from "axios";
-// import {createImage}
+import { useRouter } from "next/navigation";
+import {
+  ImageType,
+  ProductType,
+  DrinkerType,
+  SpaceType,
+  StaffType,
+} from "@utils/types/index";
+import { v4 as uuidv4 } from "uuid";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { RefreshCw } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+const EachCategory = ({
+  title,
+  tags,
+}: {
+  title: string;
+  tags: {
+    label: string;
+    value: any;
+  }[];
+}) => {
+  const colorArr = [
+    "bg-red-200",
+    "bg-yellow-200",
+    "bg-blue-200",
+    "bg-green-200",
+    "bg-purple-200",
+    "bg-pink-200",
+    "bg-indigo-200",
+    "bg-cyan-200",
+    "bg-teal-200",
+  ];
+
+  return (
+    <div className="h-fit">
+      <h3 className="mb-1 font-semibold">{title}</h3>
+      <div className="flex h-fit flex-row flex-wrap gap-1.5">
+        {tags.map((tag, index) => (
+          <Badge
+            key={index}
+            className={`${
+              colorArr[
+                Math.floor(
+                  (index + Math.random() * colorArr.length) % colorArr.length
+                )
+              ]
+            } shadow-sm hover:bg-foreground hover:text-background`}
+          >
+            {tag.label} {tag.value}
+          </Badge>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default function page() {
   const supabase = createSupabaseBrowserClient();
+
+  const [isDOpen, setIsDOpen] = useState<boolean>(false);
+  const [res, setRes] = useState<any>(null);
+
+  const router = useRouter();
 
   const [files, setFiles] = useState<File[]>([]);
 
@@ -34,22 +122,82 @@ export default function page() {
           throw new Error("Error when uploading images.");
 
         const res = await axios.post(
-          "/api/upload",
-          { link: imagesUploadResults[0] },
+          "http://localhost:3001/predict",
+          {
+            link:
+              "https://ieocflhkecudisgjolkc.supabase.co/storage/v1/object/public/images/" +
+              imagesUploadResults[0],
+          },
           {}
         );
 
         console.log("----", res);
-
-        // response -> sav to DB
+        setRes(res);
       },
       {
         loading: "Processing images...",
-        success: "Images uploaded successfully",
+        success: () => {
+          setIsDOpen(true);
+          return "Images uploaded successfully";
+        },
         error: "Error when uploading images",
       }
     );
   };
+
+  // const product_brands =
+  //   res?.data?.data?.products?.map((product: any) => {
+  //     return product.map((product: any) => {
+  //       return {
+  //         brand: {
+  //           ...product.brand,
+  //         },
+  //         type: product.type,
+  //       };
+  //     }
+  //   }) ?? [];
+
+  const drinkers =
+    res?.data?.data?.drinkers?.map((drinker: any) => {
+      return {
+        position: {
+          ...drinker.position,
+        },
+        value: drinker.value,
+      };
+    }) ?? [];
+
+  const staffs =
+    res?.data?.data?.drinkers?.map((drinker: any) => {
+      return {
+        position: {
+          ...drinker.position,
+        },
+        value: drinker.value,
+      };
+    }) ?? [];
+
+  const spaces = Object.keys(res?.data?.data?.spaces ?? {}).map((key) => {
+    return {
+      label: key,
+      value: parseFloat(res?.data?.data?.spaces[key]).toFixed(2),
+    };
+  });
+
+  const products =
+    res?.data?.data?.products?.map((product: any) => {
+      return {
+        position: {
+          ...product.position,
+        },
+        brand: {
+          ...product.brand,
+        },
+        type: product.type,
+      };
+    }) ?? [];
+
+  console.log("----", drinkers, staffs, spaces, products);
 
   return (
     <>
@@ -76,6 +224,84 @@ export default function page() {
             onFilesChange={(files) => setFiles(files)}
           />
         </div>
+        <>
+          <Dialog open={isDOpen} onOpenChange={setIsDOpen}>
+            <DialogTrigger asChild>
+              <Button variant="outline">Information</Button>
+            </DialogTrigger>
+            <DialogContent className="!max-w-[1400px] rounded-md">
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex h-fit flex-col gap-4 !text-sm">
+                <EachCategory
+                  title="Drinker"
+                  tags={[
+                    ...drinkers?.map((drinker: DrinkerType) => {
+                      return {
+                        label: JSON.stringify(drinker.position),
+                        value: drinker.value.toFixed(2),
+                      };
+                    }),
+                  ]}
+                />
+                <EachCategory title="Brands" tags={[]} />
+                {JSON.stringify(
+                  res?.data?.data?.products?.map((product: any) => {
+                    const newProduct = product;
+                    const prod_prob = parseFloat(product.value);
+                    Object.keys(product.brands).forEach((key: string) => {
+                      newProduct.brands[key] = (
+                        parseFloat(product.brands[key]) * prod_prob
+                      ).toFixed(2);
+                    });
+
+                    return {
+                      brand: newProduct.brands,
+                      type: newProduct.type,
+                    };
+                  })
+                )}
+                {/* <EachCategory
+                  title="Brand"
+                  tags={[
+                    ...product_brands?.map((product: ProductType) => {
+                      return {
+                        label: JSON.stringify(product.brand),
+                        value: product.type,
+                      };
+                    }),
+                  ]}
+                /> */}
+                <EachCategory title="Space" tags={[...spaces]} />
+                <EachCategory
+                  title="Staff"
+                  tags={[
+                    ...staffs?.map((drinker: DrinkerType) => {
+                      return {
+                        label: JSON.stringify(drinker.position),
+                        value: drinker.value.toFixed(2),
+                      };
+                    }),
+                  ]}
+                />
+              </div>
+            </DialogContent>
+            <CardFooter className="mt-auto flex justify-between gap-4">
+              <Button className="w-1/2" variant="outline">
+                Back
+              </Button>
+              <Button className="w-1/2 text-[#FFFFFF]">
+                <RefreshCw className="mr-1 h-3.5 w-3.5" />
+                <span>Refetch</span>
+              </Button>
+            </CardFooter>
+          </Dialog>
+        </>
       </div>
     </>
   );
--- src/app/_actions/drinker.ts
+++ src/app/_actions/drinker.ts
@@ -3,6 +3,28 @@
 import createSupabaseServerClient from "@/supabase/server";
 import { DrinkerType } from "@utils/types/index";
 
+export async function createDrinker({ drinker }: { drinker: DrinkerType }) {
+  try {
+    const supabase = await createSupabaseServerClient();
+
+    const result = await supabase.from("drinker").insert(drinker);
+
+    return {
+      status: result.status,
+      statusText: result.statusText,
+      data: result.data,
+      error: result.error,
+    };
+  } catch (error: any) {
+    return {
+      status: 500,
+      statusText: "Server Error",
+      data: null,
+      error: error.message,
+    };
+  }
+}
+
 export async function getProductById({ id }: { id: string }) {
   try {
     const supabase = await createSupabaseServerClient();
